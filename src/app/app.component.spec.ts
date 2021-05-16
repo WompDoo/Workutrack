7cd1f8d12e002d75b34af366978be34ede520062
WompDoo <heigo.kure@gmail.com>
2021-05-16 20:42:57 +0300

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'workutrack'`, () => {
+  it(`should have as title 'workout-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('workutrack');
+    expect(app.title).toEqual('workout-tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('workutrack app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('workout-tracker app is running!');
   });
 });
